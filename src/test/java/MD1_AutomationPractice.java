1612dd23df8c0156ee6e51c485108c5fc0b101c9
natali-t <natija@gmail.com>
2019-08-28 10:15:50 +0300

    3. "Select orange in filter"

--- src/test/java/MD1_AutomationPractice.java
+++ src/test/java/MD1_AutomationPractice.java
@@ -30,51 +30,49 @@ public class MD1_AutomationPractice {
         //select Dresses
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.invisibilityOfAllElements());
-        driver.findElement(By.id("layered_category_8")).click();
-        //driver.findElement(By.xpath(".//li[@class='sfHoverForce']//a[@title='Dresses'][1]")).click();
-        //driver.findElement(By.xpath(".//a[@class='sf-with-ul xh-highlight']")).click();
+        driver.findElement(By.xpath("//h5//a[contains(text(), 'Dresses')]")).click();
 
         //select orange in filter
         driver.findElement(By.id("layered_id_attribute_group_13")).click();
 
-        //check if all items has orange - list?
-        //loop iekš loop - turpat loop liek assert, vai ir orange
-        //1 loop - atlasītie itemi, otrais loop krāsa un tur liek klāt assert
-        //oranžo kvadrātu jābūt tik daudz, cik piedāvājumu// salīdzina piedāvājumu skaitu ar oranžo kvadrātu skaitu
-        List<WebElement> orangeList = driver.findElements(By.xpath("//*[contains(@href, 'product#/size-s/color-orange')]"));
-        for (int i = 0; i < orangeList.size(); i++) {
-            String article = orangeList.get(i).getText();
-            System.out.println(article);
-
-        }
-
-
-        //open one and check that it has orange - assertion not working
-        driver.findElement(By.xpath("//*[contains(@href, 'product#/size-s/color-orange')][1]")).click();
-        //String text = driver.findElement(By.xpath(".//span[contains(text(), 'Orange')]")).getText();
-        //Assertions.assertEquals(text, "Orange", "Text is not equal");
-
-        //Thread.sleep(5000);
-        //WebDriverWait wait1 = new WebDriverWait(driver, 30); nav izsaukts wait
-
-        //add 2 items to cart - peec 'data-id-product'?
-        //kā pāriet uz pop up logu selenium, swithc to f-ja
-        //driver.findElement(By.xpath(.//span[contains(text(), 'Add to cart')])).click();
-        driver.findElement(By.xpath(".//a[@class='button ajax_add_to_cart_button btn btn-default'][1]")).click();
-        driver.findElement(By.xpath(".//a[@class='button ajax_add_to_cart_button btn btn-default'][1]")).click();
-
-        //get price for each item
-
-        //click cart
-        //assert that sum in cart is the same as saved
-
-
-
-
-
-
-
-        driver.quit();
+//        //check if all items has orange - list?
+//        //loop iekš loop - turpat loop liek assert, vai ir orange
+//        //1 loop - atlasītie itemi, otrais loop krāsa un tur liek klāt assert
+//        //oranžo kvadrātu jābūt tik daudz, cik piedāvājumu// salīdzina piedāvājumu skaitu ar oranžo kvadrātu skaitu
+//        List<WebElement> orangeList = driver.findElements(By.xpath("//*[contains(@href, 'product#/size-s/color-orange')]"));
+//        for (int i = 0; i < orangeList.size(); i++) {
+//            String article = orangeList.get(i).getText();
+//            System.out.println(article);
+//
+//        }
+//
+//
+//        //open one and check that it has orange - assertion not working
+//        driver.findElement(By.xpath("//*[contains(@href, 'product#/size-s/color-orange')][1]")).click();
+//        //String text = driver.findElement(By.xpath(".//span[contains(text(), 'Orange')]")).getText();
+//        //Assertions.assertEquals(text, "Orange", "Text is not equal");
+//
+//        //Thread.sleep(5000);
+//        //WebDriverWait wait1 = new WebDriverWait(driver, 30); nav izsaukts wait
+//
+//        //add 2 items to cart - peec 'data-id-product'?
+//        //kā pāriet uz pop up logu selenium, swithc to f-ja
+//        //driver.findElement(By.xpath(.//span[contains(text(), 'Add to cart')])).click();
+//        driver.findElement(By.xpath(".//a[@class='button ajax_add_to_cart_button btn btn-default'][1]")).click();
+//        driver.findElement(By.xpath(".//a[@class='button ajax_add_to_cart_button btn btn-default'][1]")).click();
+//
+//        //get price for each item
+//
+//        //click cart
+//        //assert that sum in cart is the same as saved
+//
+//
+//
+//
+//
+//
+//
+//        driver.quit();
     }
 
 
